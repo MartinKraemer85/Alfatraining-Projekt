2b22272d2baf2838a867ba32bd6352eec6f6280f
MKraemer85 <martin.kraemer@sixt.com>
2023-10-10 15:39:10 +0200

    safety

--- app.py
+++ app.py
@@ -4,13 +4,22 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from flask import Flask, request
-from datetime import time
-from Routes.Auth import *
-from Routes.Article import *
 from Model.Vinyl.Record import *
+from datetime import time
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host='127.0.0.1',
+  user="postgres",
+  password="test123!",
+  database="Plattenspass"
+)
 
 app = Flask(__name__)
 
 record = Record(title="Schnarr", artist="Muh")
 record.add_track(Track(title="Niau", length=time(minute=5, second=23)))
 print(record.dict())
+
+a = float('inf') * -1
+print(a)
