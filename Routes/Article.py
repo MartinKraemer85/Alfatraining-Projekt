359c0477d8f7acd46f1cf04102a6086892eaeadd
MKraemer85 <martin.kraemer@sixt.com>
2023-10-11 11:15:22 +0200

    safety

--- Routes/Article.py
+++ Routes/Article.py
@@ -1,19 +1,17 @@
 from flask import Blueprint
 from Helper.DbHelper import DbHelper
+
 article = Blueprint('article', __name__)
 
 
 @article.route('/article', methods=['GET'])
-def get_article() -> dict:
+def get_article() -> str:
+    """
+    Get an existing article
+
+    :return: Article
+    :rtype: str[json]
+    """
+    # TODO: Filter Kram
     db_helper = DbHelper()
-    ret = db_helper.select("record", "where artist is not null", ["artist", "title"])
-    print()
-    return ret
-# def get_article(filer: dict) -> dict:
-#     """
-#     Get an existing article
-#
-#     :return: Article
-#     :rtype: dict
-#     """
-#     return {"Schnarr": "Miau"}
+    return db_helper.select("record", "where artist is not null", ["artist", "title"])
--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
-
-from Model.Vinyl.Record import *
 from Routes.Article import article
 
 app = Flask(__name__)
 app.register_blueprint(article)
 
+# sphinx-build -b html source build
+# pip freeze > requirements.txt
 
-record = Record(title="Schnarr", artist="Muh")
-record.add_track(Track(title="Niau", length=time(minute=5, second=23)))
+# from Model.Vinyl.Record import *
+# record = Record(title="Schnarr", artist="Muh")
+# record.add_track(Track(title="Niau", length=time(minute=5, second=23)))
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
