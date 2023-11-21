05232b9278a24c6a156b7300ab96fbde4c640244
MKraemer85 <martin.kraemer@sixt.com>
2023-11-21 08:42:07 +0100

    ignore

--- config_.py
+++ config_.py
@@ -2,6 +2,9 @@ import sqlalchemy as db
 from decouple import config
 
 # make the engine available for all modules that need it
-# https://docs.sqlalchemy.org/en/20/errors.html#error-3o7r
-engine = db.create_engine(f"mssql://@{config('SERVER')}/{config('DATABASE')}?driver={config('ODBC')}",  max_overflow=-1)
+engine = db.create_engine(f"mssql://@{config('SERVER')}/{config('DATABASE')}?driver={config('ODBC')}")
+
+# sqllite fuer Projektabgabe
+# engine = db.create_engine(f"sqlite:///project.db", echo=True)
+# engine = db.create_engine(f"sqlite:///E:\repos\Plattenfreude\project.db", echo=True)
 
