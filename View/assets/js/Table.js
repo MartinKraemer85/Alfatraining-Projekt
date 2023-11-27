1c0bba408cef2136cae1c4c582e4fa42455112b2
MKraemer85 <martin.kraemer@sixt.com>
2023-11-27 15:10:51 +0100

    safety

--- View/assets/js/Table.js
+++ View/assets/js/Table.js
@@ -1,25 +1,54 @@
 
 
 'use strict';
-const rowAmounts = [5,10,25]
-const currentRowAmount = [10]
+const elements = {}
+elements.rowAmounts = [5, 10, 25]
+elements.currentRowAmount = 10
+elements.page = 1
+elements.paginationData = []
 
-const addToTable = ({ tr, tableRow, head = false , tableElement} = {}) => {
+
+const tableRowClick = (evt) => {
+    const target = evt.currentTarget;
+    const pTag = $(target.nextSibling);
+
+    $(pTag).is(":visible") ? $(pTag).slideUp()  :  $(pTag).slideDown()
+    
+    //pTag.hidden = false;
+
+}
+
+const addRowToTable = ({ tr, data, head = false, tableElement } = {}) => {
     /** 
      * Iterates over the given table row 
-     * @tr {tableRow} 
-     * @tableRow {object} 
-     * @head {bool}  
+     * @tr {domObject} the current table row we want to fill / add
+     * @data {object} the data object like an article i.e. {"artist" : "a artist", "year": "a year"}
+     * @head {bool}  determines if head or body element
+     * @tableElement {domObject} the element where to append the row to. Can be the tableHead or tableBody
     */
-    for (const [key, value] of Object.entries(tableRow)) {
-        // Ignore relationships like the genre and ignore the article id also
-        if (typeof value =='object') continue;
-        if (key.match('id')) continue;
+    tr.addEventListener('click', tableRowClick) 
+
+    for (const [key, value] of Object.entries(data)) {
+        // Ignore relationships like the genre
+        if (typeof value == 'object') continue;
+        if (key.match('state')) continue;
 
         const thtd = document.createElement(head ? "th" : "td");
+        
+        if (key.match('id')) thtd.hidden = true;
+
         thtd.innerHTML = head ? key : value
+
+
         tr.append(thtd);
         tableElement.append(tr);
+    }
+
+    if (!head){
+        const infoContainer = document.createElement('p')
+        infoContainer.hidden = true
+        infoContainer.innerHTML = "Hier kommen Daten herein"
+        tr.after(infoContainer)
     }
 }
 
@@ -30,21 +59,21 @@ const filterData = (filterArr, tableData) => {
      * If this is the case, check if the filters array includes the genre name
      * Filter genre and sub genre aswell
     */
-    
-    return filterArr.length ? tableData.filter(article =>{
+
+    return filterArr.length ? tableData.filter(article => {
         const ret = 0;
         return article.genres.some(genre => {
             return filterArr.some(filter => {
                 if (genre.genre.name == filter) return true
                 return false;
-            }) ;
+            });
         }) || article.sub_genres.some(sub_genre => {
             return filterArr.some(filter => {
                 if (sub_genre.sub_genre.name == filter) return true
                 return false;
             });
-        }) 
-    } ) : tableData
+        })
+    }) : tableData
 
 }
 
@@ -55,55 +84,81 @@ const removeElements = (el) => {
     }
 }
 
-
-const addButtonDiv = (tableBody, tr, cssClass="tableBtnLeft") => {
-    const buttonDiv = document.createElement('div')
-    buttonDiv.classList.add(cssClass)
-    tr.append(buttonDiv)
-    tableBody.append(tr)
-    return buttonDiv;
-}
-
-const addTableFooter = (tableHead, tableBody, tableData, filter) => {
-    // create a new tr where the divs go in
-    const tr = document.createElement("tr");
-    // the buttons for the table amount (display 5,10,xx at once)
-    // create a new table row first, and place the buttons within the row
-    const amountDiv = addButtonDiv(tableBody, tr)
-    rowAmounts.forEach(value => {
+const addRowAmount = (tableHead, tableBody, tableFoot, tableData, filter, tr, colspan) => {
+    const tdLeft = document.createElement("td")
+    tdLeft.colSpan= Math.floor(colspan / 2);
+    tdLeft.classList.add("tableBtnLeft")
+    tr.append(tdLeft)
+    
+    // Displayed row amount:
+    // iterate over the row amounts array (how many rows are displayed in the table)
+    // for each entrie, add an button + event listener
+    elements.rowAmounts.forEach(value => {
         const tableBtn = document.createElement('button')
-
         tableBtn.innerText = value
         tableBtn.addEventListener('click', (event) => {
-            // Delete the current amount of rows displayed
-            currentRowAmount.pop()
-            currentRowAmount.push(Number(event.target.innerText))
-            table(tableHead, tableBody, tableData, filter)
+            // set the new row amount 
+            elements.currentRowAmount = Number(event.target.innerText)
+            // also reset the page
+            elements.page = 1
+            table(tableHead, tableBody, tableFoot, tableData, filter)
         })
-        amountDiv.append(tableBtn)
-    })
+        tdLeft.append(tableBtn)
+    })    
+}
 
-    const numberBtnDiv = addButtonDiv(tableBody, tr, "tableBtnRight")
-    numberBtnDiv.style.right = '0px';
-    
-    for (let i =1; i<=Math.ceil(tableData.length/ currentRowAmount); i++){
-        const tableBtn = document.createElement('button')
-        tableBtn.innerText = i
-        tableBtn.addEventListener('click', (event) => {
-            table(tableHead, tableBody, tableData, filter)
+const addPagination = (tableHead, tableBody, tableFoot, tableData, filter, tr, colspan) => {
+    const tdRight = document.createElement("td")
+    tdRight.colSpan = Math.ceil(colspan / 2);
+    tdRight.classList.add("tableBtnRight")
+    tr.append(tdRight)
+
+    // Pagination:
+    // First, get the pagination amount by dividing the tabledata with the current amount of displayed
+    // items. 
+    // Use ceil to allways round up
+    const paginationAmount = Math.ceil(tableData.length / elements.currentRowAmount)
+    for (let i = 1; i <= paginationAmount; i++) {
+        // create the pagination buttons and add them to the right column of the 
+        // table footer element
+        const paginationBtn = document.createElement('button')
+        paginationBtn.innerText = i
+        paginationBtn.addEventListener('click', (event) => {
+            elements.pagination = Number(event.target.innerHTML)
+
+            elements.page = Number(event.target.innerHTML)
+            //render table again, but only use the data from the given page
+            table(tableHead, tableBody, tableFoot, tableData, filter)
         })
-        numberBtnDiv.append(tableBtn)
+        tdRight.append(paginationBtn)
     }
+    tableFoot.append(tr)
+}
 
+const addTableFooter = (tableHead, tableBody, tableFoot, tableData, filter) => {
+    // create a new table row first, and place the buttons within the row later on
+    const tr = document.createElement("tr");
+
+
+    // What we also need to do is setting the coolspan.
+    // this way we can make sure that the buttons in the footer 
+    // are in the correct place aka the row amount buttons left,
+    // the pagination right, since we have only 2 rows over the whole table this way.
+    const colspan = tableHead.rows[0].cells.length;
+
+    // the buttons for the table amount (display 5,10,xx at once)
+    addRowAmount(tableHead, tableBody, tableFoot, tableData, filter, tr, colspan)
+    addPagination(tableHead, tableBody, tableFoot, tableData, filter, tr, colspan)
 
 }
 
-const table = (tableHead, tableBody, tableData, filterArr) => {
+const table = (tableHead, tableBody, tableFoot, tableData, filterArr) => {
     /** 
      * Appends a table to a table head / body tag, and fills the table with the given data
      * 
      * @tabbleHead {domElement} the table head where the table should be append to
      * @tableBody {domElement} the table body element where the table should be append to
+     * @tableFooter {domElement} the table footer element where the table should be append to
      * @tableData {Array[dict]} the data to fill the table
      * @filter {Array} the data to fill the table
      * i.e. [{
@@ -169,22 +224,34 @@ const table = (tableHead, tableBody, tableData, filterArr) => {
     */
     removeElements(tableHead);
     removeElements(tableBody);
-    let tr = document.createElement("tr");
-    let count = 0;
-    for (const article of filterData(filterArr,tableData)) {
-        // only render the amount the user has set
-        if (count == currentRowAmount[0]) break
-        if (!count) {
-            addToTable({ tr, tableRow: article, head: true, tableElement: tableHead})
-        }
-        tr = document.createElement("tr");
+    removeElements(tableFoot);
 
-        addToTable({ tr, tableRow: article, head: false, tableElement: tableBody })
-        count ++;
+    
+    let count = 0;
+    const filteredData = filterData(filterArr, tableData)
+    
+    // calcualte the displayed data, since we only need to iterate over the data we want to display
+    // Data depends on the page and row amount displayed
+    const from = (elements.page-1) * elements.currentRowAmount
+    const to = elements.page * elements.currentRowAmount
+    const pageData = tableData.slice(from,to)
+
+    for (const article of pageData) {
+        // only render the amount the user has set
+        if (count == elements.currentRowAmount) break
+        if (!count) {
+            const tr = document.createElement("tr");
+            addRowToTable({ tr, data: article, head: true, tableElement: tableHead })
+        }
+        const tr = document.createElement("tr");
+
+        addRowToTable({ tr, data: article, head: false, tableElement: tableBody })
+        count++;
     }
-    addTableFooter(tableHead, tableBody, tableData, filterArr)
+    console.log(tableData);
+    addTableFooter(tableHead, tableBody, tableFoot, tableData, filterArr)
 }
 
 
 
-export { table, rowAmounts, currentRowAmount };
+export { table };
