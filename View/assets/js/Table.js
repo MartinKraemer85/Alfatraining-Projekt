c8340e01258a5e30f8d0548703f3950572b54cc5
MKraemer85 <martin.kraemer@sixt.com>
2023-11-28 12:26:46 +0100

    shopping cart button schmarn

--- View/assets/js/Table.js
+++ View/assets/js/Table.js
@@ -7,41 +7,121 @@ elements.currentRowAmount = 10
 elements.page = 1
 elements.paginationData = []
 
+// covers not implemented in the backend yet, so just take some piggi pics
+elements.images = [
+    "./assets/images/bertram.jpg",
+    "./assets/images/bocchi.jpg",
+    "./assets/images/bocchi2.jpg",
+    "./assets/images/momo.jpg",
+    "./assets/images/momo2.jpg",
+]
+const createNumber = (min, max) => ~~(Math.random() * (max - min + 1) + min);
 
 const tableRowClick = (evt) => {
     const target = evt.currentTarget;
     // convert to jQuery for the usage of slideUp /Down
     const pTag = $(target.nextSibling);
 
-    $(pTag).is(":visible") ? $(pTag).slideUp('fast')  :  $(pTag).slideDown('fast')
-    
-    //pTag.hidden = false;
+    $(pTag).is(":visible") ? $(pTag).slideUp('fast') : $(pTag).slideDown('fast')
 
 }
 
-const addTrackInfo = (head, data, tableElement) =>{
-    // ignore the head data, obviously
-    if (!head){
-        console.log(tableElement);
-        const row = document.createElement('tr')
-        const td = document.createElement('td')
-        const infoContainer = document.createElement('p')
-        const trackList = document.createElement('ul')
-        
-        td.colSpan = elements.colspan;
-        row.hidden = true
-        //infoContainer.classList.add("pTag")
-        data.tracks.forEach(track => {
-            const liTrack = document.createElement("li")
-            liTrack.innerText = `${track.title} (${track.length})`
-            trackList.append(liTrack)
-        })
+const addToCartButton = (tr, head) => {
+    /** 
+ * Takes the current row (if not the head) and adds the to cart button
+ * @tr {domObject} the current table row we want to fill / add
+ * @head {bool}  determines if head or body element 
+ * */
+    if (head) {
+        tr.append(document.createElement("th"))
 
-        infoContainer.append(trackList)
-        td.append(infoContainer)
-        row.append(td)
-        tableElement.append(row)
-    }
+        return
+    };
+
+    const td = document.createElement("td")
+    const btn = document.createElement("button")
+    const iTag = document.createElement("i")
+        
+    iTag.classList.add("fa")
+    iTag.classList.add("fa-shopping-cart")
+    //iTag.classList.add("fa")
+    //iTag.classList.add("fa-home")
+    btn.classList.add("addtocartBtn")
+
+
+    btn.append(iTag)
+    td.append(btn)
+    tr.append(td)
+
+}
+
+const addCover = (tr, td, data, tableElement) => {
+    /** 
+     * Adding a cover to the info Box
+     *  
+     * @tr {domObject} the current table row we want to fill / add to
+     * @td {domObject} the currend td, with colSpan = header items
+     * @data {object} the data object like an article i.e. {"artist" : "a artist", "year": "a year"}
+     * @tableElement {domObject} the element where to append the row to(tableHead/tableBody)
+    */
+    const infoContainer = document.createElement('div')
+    infoContainer.classList.add("infoDiv");
+    const img = document.createElement("img")
+
+    // for now, random image from an array, later on from the data object
+    img.src = elements.images[createNumber(0, elements.images.length - 1)]
+    img.classList.add("cover")
+    infoContainer.classList.add("infoDiv")
+    infoContainer.append(img)
+    td.append(infoContainer)
+    tr.append(td)
+    tableElement.append(tr)
+}
+
+const addTrackInfo = (tr, td, data, tableElement) => {
+    /** 
+     * Iterate the element.tracks objects and add them to a table row that will be appended
+     * after the actual row is appended
+     *  
+     * @tr {domObject} the current table row we want to fill / add to
+     * @td {domObject} the currend td, with colSpan = header items
+     * @data {object} the data object like an article i.e. {"artist" : "a artist", "year": "a year"}
+     * @tableElement {domObject} the element where to append the row to(tableHead/tableBody)
+    */
+
+    const infoContainer = document.createElement('div')
+    const trackList = document.createElement('ul')
+
+    infoContainer.classList.add("infoDiv")
+    data.tracks.forEach(track => {
+        const liTrack = document.createElement("li")
+        liTrack.innerText = `${track.title} (${track.length})`
+        trackList.append(liTrack)
+    })
+
+    infoContainer.append(trackList)
+    td.append(infoContainer)
+    tr.append(td)
+    tableElement.append(tr)
+
+}
+
+const addVendorInfo = (tr, td, data, tableElement) => {
+    /** 
+     * Iterate the element.tracks objects and add them to a table row that will be appended
+     * after the actual row is appended
+     *  
+     * @tr {domObject} the current table row we want to fill / add to
+     * @td {domObject} the currend td, with colSpan = header items
+     * @data {object} the data object like an article i.e. {"artist" : "a artist", "year": "a year"}
+     * @tableElement {domObject} the element where to append the row to(tableHead/tableBody)
+    */
+    const infoContainer = document.createElement('div')
+    infoContainer.classList.add("infoDiv")
+    infoContainer.innerText = "todo: Händerfreude"
+    td.append(infoContainer)
+    tr.append(td)
+    tableElement.append(tr)
 }
 
 const addRowToTable = ({ tr, data, head = false, tableElement } = {}) => {
@@ -50,9 +130,9 @@ const addRowToTable = ({ tr, data, head = false, tableElement } = {}) => {
      * @tr {domObject} the current table row we want to fill / add
      * @data {object} the data object like an article i.e. {"artist" : "a artist", "year": "a year"}
      * @head {bool}  determines if head or body element
-     * @tableElement {domObject} the element where to append the row to. Can be the tableHead or tableBody
+     * @tableElement {domObject} the element where to append the row to(tableHead/tableBody)
     */
-    tr.addEventListener('click', tableRowClick) 
+    tr.addEventListener('click', tableRowClick)
 
     for (const [key, value] of Object.entries(data)) {
         // Ignore relationships like the genre
@@ -60,7 +140,7 @@ const addRowToTable = ({ tr, data, head = false, tableElement } = {}) => {
         if (key.match('state')) continue;
 
         const thtd = document.createElement(head ? "th" : "td");
-        
+
         if (key.match('id')) thtd.hidden = true;
 
         thtd.innerHTML = head ? key : value
@@ -69,11 +149,20 @@ const addRowToTable = ({ tr, data, head = false, tableElement } = {}) => {
         tableElement.append(tr);
     }
 
+    addToCartButton(tr, head)
+
+    // InfoRow:
     // Add the tracklist and maybe other information to an slidable
     // <p> tag
+    const infoRow = document.createElement('tr')
+    infoRow.classList.add("infoTr")
+    const td = document.createElement('td')
+    td.colSpan = elements.colspan;
+    infoRow.hidden = true
 
-    addTrackInfo(head, data, tableElement)
-
+    if (!head) addCover(infoRow, td, data, tableElement)
+    if (!head) addTrackInfo(infoRow, td, data, tableElement)
+    if (!head) addVendorInfo(infoRow, td, data, tableElement)
 }
 
 const filterData = (filterArr, tableData) => {
@@ -110,10 +199,10 @@ const removeElements = (el) => {
 
 const addRowAmount = (tableHead, tableBody, tableFoot, tableData, filter, tr) => {
     const tdLeft = document.createElement("td")
-    tdLeft.colSpan= Math.floor(elements.colspan / 2);
+    tdLeft.colSpan = Math.floor(elements.colspan / 2);
     tdLeft.classList.add("tableBtnLeft")
     tr.append(tdLeft)
-    
+
     // Displayed row amount:
     // iterate over the row amounts array (how many rows are displayed in the table)
     // for each entrie, add an button + event listener
@@ -128,7 +217,7 @@ const addRowAmount = (tableHead, tableBody, tableFoot, tableData, filter, tr) =>
             table(tableHead, tableBody, tableFoot, tableData, filter)
         })
         tdLeft.append(tableBtn)
-    })    
+    })
 }
 
 const addPagination = (tableHead, tableBody, tableFoot, tableData, filter, tr) => {
@@ -249,15 +338,15 @@ const table = (tableHead, tableBody, tableFoot, tableData, filterArr) => {
     removeElements(tableBody);
     removeElements(tableFoot);
 
-    
+
     let count = 0;
     const filteredData = filterData(filterArr, tableData)
-    
+
     // calcualte the displayed data, since we only need to iterate over the data we want to display
     // Data depends on the page and row amount displayed
-    const from = (elements.page-1) * elements.currentRowAmount
+    const from = (elements.page - 1) * elements.currentRowAmount
     const to = elements.page * elements.currentRowAmount
-    const pageData = filteredData.slice(from,to)
+    const pageData = filteredData.slice(from, to)
 
     for (const article of pageData) {
         // only render the amount the user has set
